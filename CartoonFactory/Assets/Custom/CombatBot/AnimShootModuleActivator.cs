51b3ace20772b0231f4a1d493b03dc89b9468c2d
Igo_Go <chudakin98@gmail.com>
2020-12-12 20:46:30 +0700

    Анимации робота

---
+++ CartoonFactory/Assets/Custom/CombatBot/AnimShootModuleActivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimShootModuleActivator : MonoBehaviour
+{
+    public ShootModule rightShoot;
+    public ShootModule leftShoot;
+
+    public void StartShootForRight() => rightShoot.AnimStartShoot();
+
+    public void StartShootForLeft() => leftShoot.AnimStartShoot();
+
+    public void StopShootForRight() => rightShoot.AnimStopShoot();
+
+    public void StopShootForLeft() => leftShoot.AnimStopShoot();
+}
